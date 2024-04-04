a03507063031fe371493b5406989405f714a73d6
Bryce Ellet <brotherbattles2@gmail.com>
2024-04-04 11:34:24 -0400

    Git set up

--- FoodSelector/HalfVC.swift
+++ FoodSelector/HalfVC.swift
@@ -10,7 +10,6 @@ import CoreLocation
 
 class HalfVC: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
     // TODO: fix the open for venues
-    // TODO: set up git
     // TODO: fix the search order so you click correct venue
     // TODO: fix the constraints
     // TODO: fix the venue view height in the infoVC
